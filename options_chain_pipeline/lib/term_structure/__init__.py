02242e48faa95a2f9116362f106f3d552cbd4f29
Sam Green <59747859+sgreen7979@users.noreply.github.com>
2024-08-10 15:12:09 -0400

    Update __init__.py

--- options_chain_pipeline/lib/term_structure/__init__.py
+++ options_chain_pipeline/lib/term_structure/__init__.py
@@ -1,15 +1,14 @@
-from .base import SimpleYieldTermStructure as SimpleYieldTermStructure
-from .base import YieldTermStructure as YieldTermStructure
-from .cubic_spline import CubicSplineYieldTermStructure as CubicSplineYieldTermStructure
-from .flat_forward import FlatForwardYieldTermStructure as FlatForwardYieldTermStructure
-from .flat import FlatYieldTermStructure as FlatYieldTermStructure
+#!/usr/bin/env python3
+from .base import SimpleYieldTermStructure as SimpleYieldTermStructure  # noqa
+from .base import YieldTermStructure as YieldTermStructure  # noqa
+from .cubic_spline import CubicSplineYieldTermStructure as CubicSplineYieldTermStructure  # noqa
+from .flat_forward import FlatForwardYieldTermStructure as FlatForwardYieldTermStructure  # noqa
+from .flat import FlatYieldTermStructure as FlatYieldTermStructure  # noqa
 from .nelson_siegel import (
     NelsonSiegelYieldTermStructure as NelsonSiegelYieldTermStructure,
-)
+)  # noqa
 from .piecewise import (
     PiecewiseLinearYieldTermStructure as PiecewiseLinearYieldTermStructure,
-)
-from .svenson import SvenssonYieldTermStructure as SvenssonYieldTermStructure
-from .market_data import get_market_data as get_market_data
-
-# # from .lib import yieldtermstructure
+)  # noqa
+from .svenson import SvenssonYieldTermStructure as SvenssonYieldTermStructure  # noqa
+from .market_data import get_market_data as get_market_data  # noqa
