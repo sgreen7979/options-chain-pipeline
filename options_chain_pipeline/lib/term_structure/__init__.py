a3418047aca5adbd4d855d5ac726773cbc3ba29b
Sam Green <59747859+sgreen7979@users.noreply.github.com>
2024-08-10 15:13:22 -0400

    Update __init__.py

--- options_chain_pipeline/lib/term_structure/__init__.py
+++ options_chain_pipeline/lib/term_structure/__init__.py
@@ -2,8 +2,9 @@
 from .base import SimpleYieldTermStructure as SimpleYieldTermStructure  # noqa
 from .base import YieldTermStructure as YieldTermStructure  # noqa
 from .cubic_spline import CubicSplineYieldTermStructure as CubicSplineYieldTermStructure  # noqa
-from .flat_forward import FlatForwardYieldTermStructure as FlatForwardYieldTermStructure  # noqa
 from .flat import FlatYieldTermStructure as FlatYieldTermStructure  # noqa
+from .flat_forward import FlatForwardYieldTermStructure as FlatForwardYieldTermStructure  # noqa
+from .market_data import get_market_data as get_market_data  # noqa
 from .nelson_siegel import (
     NelsonSiegelYieldTermStructure as NelsonSiegelYieldTermStructure,
 )  # noqa
@@ -11,4 +12,3 @@ from .piecewise import (
     PiecewiseLinearYieldTermStructure as PiecewiseLinearYieldTermStructure,
 )  # noqa
 from .svenson import SvenssonYieldTermStructure as SvenssonYieldTermStructure  # noqa
-from .market_data import get_market_data as get_market_data  # noqa
